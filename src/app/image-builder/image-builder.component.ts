ece7f6e8c73a4b327b685708317377b162b3424c
Crypto Dev <herts92@hotmail.com>
2023-04-25 05:00:18 -0500

    imageMap isolated

--- src/app/image-builder/image-builder.component.ts
+++ src/app/image-builder/image-builder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
+import { imageMap } from './imageMap';
 
 interface ImageMap {
   [category: string]: {
@@ -71,75 +72,10 @@ export class ImageBuilderComponent implements OnInit {
 
   loadImages(category: keyof ImageMap) {
     console.log("category", category);
-    const imageMap: ImageMap = {
-      vagina: [ // cuales son las imagenes?
-        {id: "vagina1", imageUrl: '../assets/VULVATAR_ELEMENTOS/BASES/01.png'},
-        {id: "vagina2", imageUrl: '../assets/VULVATAR_ELEMENTOS/BASES/01.png'},
-        {id: "vagina3", imageUrl: '../assets/VULVATAR_ELEMENTOS/BASES/01.png'}
-      ],
-      labios: [ // ok
-        {id: "labios1", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/01.png'},
-        {id: "labios2", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/02.png'},
-        {id: "labios3", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/03.png'},
-        {id: "labios4", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/04.png'},
-        {id: "labios5", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/05.png'},
-        {id: "labios6", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/06.png'},
-        {id: "labios7", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/07.png'},
-        {id: "labios8", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/08.png'},
-        {id: "labios9", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/09.png'},
-        {id: "labios10", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/10.png'}
-      ],
-      clitoris: [ // ok
-        {id: "clitoris1", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/01.png'},
-        {id: "clitoris2", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/02.png'},
-        {id: "clitoris3", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/03.png'},
-        {id: "clitoris4", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/04.png'},
-        {id: "clitoris5", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/05.png'},
-        {id: "clitoris6", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/06.png'},
-        {id: "clitoris7", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/07.png'},
-        {id: "clitoris8", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/08.png'},
-        {id: "clitoris9", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/09.png'},
-        {id: "clitoris10", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/10.png'}
-      ],
-      vello_pubico: [ // TODO. Preguntar diseño como sera el orden para mostrar porque son muchas imagenes
-        {id: "vello_pubico1", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/1/vellos color_1-01.png'},
-        {id: "vello_pubico2", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/1/vellos color_1-02.png'},
-        {id: "vello_pubico3", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/1/vellos color_1-03.png'},
-        {id: "vello_pubico4", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/1/vellos color_1-04.png'},
-        {id: "vello_pubico5", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/1/vellos color_1-05.png'},
-        {id: "vello_pubico6", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/1/vellos color_1-06.png'},
-        {id: "vello_pubico7", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/1/vellos color_1-07.png'},
-        {id: "vello_pubico8", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/1/vellos color_1-08.png'},
-        {id: "vello_pubico9", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/1/vellos color_1-09.png'},
-        {id: "vello_pubico10", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/1/vellos color_1-10.png'}
-      ],
-      accesorios: [ // ok
-        {id: "accesorios1", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/01.png'},
-        {id: "accesorios2", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/02.png'},
-        {id: "accesorios3", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/03.png'},
-        {id: "accesorios4", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/04.png'},
-        {id: "accesorios5", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/05.png'},
-        {id: "accesorios6", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/06.png'},
-        {id: "accesorios7", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/07.png'},
-        {id: "accesorios8", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/08.png'},
-        {id: "accesorios9", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/09.png'},
-        {id: "accesorios10", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/10.png'}
-      ],
-      color_piel: [ // ok
-        {id: "color_piel1", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/01.png'},
-        {id: "color_piel2", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/02.png'},
-        {id: "color_piel3", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/03.png'},
-        {id: "color_piel4", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/04.png'},
-        {id: "color_piel5", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/05.png'},
-        {id: "color_piel6", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/06.png'},
-        {id: "color_piel7", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/07.png'},
-        {id: "color_piel8", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/08.png'},
-        {id: "color_piel9", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/09.png'},
-        {id: "color_piel10", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/10.png'}
-      ]
-    };
+    
     // TODO no se que es COLOR_PIEL_PSI
     // TODO no se que es EXTRAS_Fondos_Vulvatars
+    // TODO cuales son los valores por defecto que cargan la primera vez
     this.selectedCategory = category;
     this.images = imageMap[category];
   }
