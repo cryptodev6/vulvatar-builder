9a1c0d43f93701ec13c5de071145d156d653c160
Crypto Dev <herts92@hotmail.com>
2023-04-25 04:25:39 -0500

    status modified

--- src/app/image-builder/image-builder.component.ts
+++ src/app/image-builder/image-builder.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 
 interface ImageMap {
   [category: string]: {
-    id: number,
+    id: string,
     imageUrl: string
   }[];
 }
@@ -73,69 +73,69 @@ export class ImageBuilderComponent implements OnInit {
     console.log("category", category);
     const imageMap: ImageMap = {
       vagina: [
-        {id: 1, imageUrl: 'vagina-image-1.jpg'},
-        {id: 2, imageUrl: 'vagina-image-2.jpg'},
-        {id: 3, imageUrl: 'vagina-image-3.jpg'}
+        {id: "vagina1", imageUrl: 'vagina-image-1.jpg'},
+        {id: "vagina2", imageUrl: 'vagina-image-2.jpg'},
+        {id: "vagina3", imageUrl: 'vagina-image-3.jpg'}
       ],
       labios: [
-        {id: 1, imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/01.png'},
-        {id: 2, imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/02.png'},
-        {id: 3, imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/03.png'},
-        {id: 4, imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/04.png'},
-        {id: 5, imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/05.png'},
-        {id: 6, imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/06.png'},
-        {id: 7, imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/07.png'},
-        {id: 8, imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/08.png'},
-        {id: 9, imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/09.png'},
-        {id: 10, imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/10.png'}
+        {id: "labios1", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/01.png'},
+        {id: "labios2", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/02.png'},
+        {id: "labios3", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/03.png'},
+        {id: "labios4", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/04.png'},
+        {id: "labios5", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/05.png'},
+        {id: "labios6", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/06.png'},
+        {id: "labios7", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/07.png'},
+        {id: "labios8", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/08.png'},
+        {id: "labios9", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/09.png'},
+        {id: "labios10", imageUrl: '../assets/VULVATAR_ELEMENTOS/LABIOS/10.png'}
       ],
       clitoris: [
-        {id: 1, imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/01.png'},
-        {id: 2, imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/02.png'},
-        {id: 3, imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/03.png'},
-        {id: 4, imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/04.png'},
-        {id: 5, imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/05.png'},
-        {id: 6, imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/06.png'},
-        {id: 7, imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/07.png'},
-        {id: 8, imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/08.png'},
-        {id: 9, imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/09.png'},
-        {id: 10, imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/10.png'}
+        {id: "clitoris1", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/01.png'},
+        {id: "clitoris2", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/02.png'},
+        {id: "clitoris3", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/03.png'},
+        {id: "clitoris4", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/04.png'},
+        {id: "clitoris5", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/05.png'},
+        {id: "clitoris6", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/06.png'},
+        {id: "clitoris7", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/07.png'},
+        {id: "clitoris8", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/08.png'},
+        {id: "clitoris9", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/09.png'},
+        {id: "clitoris10", imageUrl: '../assets/VULVATAR_ELEMENTOS/CLITORIS/10.png'}
       ],
       vello_pubico: [
-        {id: 1, imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/01.png'},
-        {id: 2, imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/02.png'},
-        {id: 3, imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/03.png'},
-        {id: 4, imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/04.png'},
-        {id: 5, imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/05.png'},
-        {id: 6, imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/06.png'},
-        {id: 7, imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/07.png'},
-        {id: 8, imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/08.png'},
-        {id: 9, imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/09.png'},
-        {id: 10, imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/10.png'}
+        {id: "vello_pubico1", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/01.png'},
+        {id: "vello_pubico2", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/02.png'},
+        {id: "vello_pubico3", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/03.png'},
+        {id: "vello_pubico4", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/04.png'},
+        {id: "vello_pubico5", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/05.png'},
+        {id: "vello_pubico6", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/06.png'},
+        {id: "vello_pubico7", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/07.png'},
+        {id: "vello_pubico8", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/08.png'},
+        {id: "vello_pubico9", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/09.png'},
+        {id: "vello_pubico10", imageUrl: '../assets/VULVATAR_ELEMENTOS/VELLO_PUBICO/10.png'}
       ],
       accesorios: [
-        {id: 1, imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/01.png'},
-        {id: 2, imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/02.png'},
-        {id: 3, imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/03.png'},
-        {id: 4, imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/04.png'},
-        {id: 5, imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/05.png'},
-        {id: 6, imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/06.png'},
-        {id: 7, imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/07.png'},
-        {id: 8, imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/08.png'},
-        {id: 9, imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/09.png'},
-        {id: 10, imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/10.png'}
+        {id: "accesorios1", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/01.png'},
+        {id: "accesorios2", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/02.png'},
+        {id: "accesorios3", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/03.png'},
+        {id: "accesorios4", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/04.png'},
+        {id: "accesorios5", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/05.png'},
+        {id: "accesorios6", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/06.png'},
+        {id: "accesorios7", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/07.png'},
+        {id: "accesorios8", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/08.png'},
+        {id: "accesorios9", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/09.png'},
+        {id: "accesorios10", imageUrl: '../assets/VULVATAR_ELEMENTOS/ACCESORIOS/10.png'}
       ],
       color_piel: [
-        {id: 1, imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/01.png'},
-        {id: 2, imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/02.png'},
-        {id: 3, imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/03.png'},
-        {id: 4, imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/04.png'},
-        {id: 5, imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/05.png'},
-        {id: 6, imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/06.png'},
-        {id: 7, imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/07.png'},
-        {id: 8, imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/08.png'},
-        {id: 9, imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/09.png'},
-        {id: 10, imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/10.png'}
+        {id: "color_piel1", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/01.png'},
+        {id: "color_piel2", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/02.png'},
+        {id: "color_piel3", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/03.png'},
+        {id: "color_piel4", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/04.png'},
+        {id: "color_piel5", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/05.png'},
+        {id: "color_piel6", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/06.png'},
+        {id: "color_piel7", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/07.png'},
+        {id: "color_piel8", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/08.png'},
+        {id: "color_piel9", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/09.png'},
+        {id: "color_piel10", imageUrl: '../assets/VULVATAR_ELEMENTOS/COLOR_PIEL/10.png'}
       ]
     };
     this.selectedCategory = category;
@@ -143,24 +143,25 @@ export class ImageBuilderComponent implements OnInit {
   }
 
   selectImage(image: any) {
-    /*const img = new Image();
-    img.src = image.imageUrl;
-    img.className = 'image-canvas';
-    this.el.nativeElement.querySelector('#canvas').appendChild(img);*/
+    for (var i=0; i<= this.selectedImages.length; i++) {
+      if (this.selectedImages[i].category === this.selectedCategory) {
+        this.selectedImages[i].selectedId = image.id;
+        this.appendDiv(image.imageUrl, image.id)
+        break;
+      } else {
+        
+      }
 
-     const img = document.createElement('img');
-    img.src = image.imageUrl;
+    }
+  }
+
+  appendDiv(imageUrl: string, imageId: string) {
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.id = imageId;
     img.style.position = 'fixed';
     img.style.height = '400px';
     const canvas = document.querySelector("#canvas");
     canvas?.appendChild(img);
-
-
-    for (var i=0; i<= this.selectedImages.length; i++) {
-      if (this.selectedImages[i].category === this.selectedCategory) {
-        this.selectedImages[i].selectedId = image.id;
-        break;
-      }
-    }
   }
 }
